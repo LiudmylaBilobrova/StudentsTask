95635a34cf67df47cd25f1fcbbeb7ca2c646427e
Lyudmila.Bilobrova <bilobrova81@gmail.com>
2021-01-05 15:14:39 +0200

    whole task

---
+++ StudentsListTask/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsListTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student("Aristarh") {BirthYear = 2003, Age = 18 };
+            var student2 = new Student("Innokentiy") { BirthYear = 2002, Age = 19 };
+            var student3 = new Student("Edik") { BirthYear = 2004, Age = 17 };
+
+        }
+    }
+}
---
+++ StudentsListTask/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsListTask
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public int Age { get; set; }
+
+        public Student(string studentName)
+        {
+            this.Name = studentName;
+        }
+    }
+}
